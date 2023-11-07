55dd6121f94185a20ac8052a8052b225de690faa
Luigui Marco Lupacca Apaza <luigui.lupacca@tecsup.edu.pe>
2023-11-07 17:58:48 -0500

    laboratorio completex

--- Snapchat/AppDelegate.swift
+++ Snapchat/AppDelegate.swift
@@ -8,15 +8,22 @@
 import UIKit
 import CoreData
 import FirebaseCore
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    func application(_ app: UIApplication,
+                     open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+      return GIDSignIn.sharedInstance.handle(url)
+    }
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         return true
     }
+    
+    
 
     // MARK: UISceneSession Lifecycle
 
--- Snapchat/iniciarSesionViewController.swift
+++ Snapchat/iniciarSesionViewController.swift
@@ -6,7 +6,9 @@
 //
 
 import UIKit
+import FirebaseCore
 import FirebaseAuth
+import GoogleSignIn
 
 class iniciarSesionViewController: UIViewController {
 
@@ -14,6 +16,7 @@ class iniciarSesionViewController: UIViewController {
     
     @IBOutlet weak var passwordTextField: UITextField!
     
+    @IBOutlet weak var botonGoogle: UIButton!
     @IBAction func iniciarSesionTapped(_ sender: Any) {
         Auth.auth().signIn(withEmail: emailTextField.text!,password: passwordTextField.text!){ (user,error) in print("Intentado Iniciar sesion")
             if error != nil{
@@ -23,10 +26,41 @@ class iniciarSesionViewController: UIViewController {
             }
         }
     }
+    @IBAction func iniciarSesionGoogleTapped(_ sender: Any) {
+        // Configura Google Sign-In
+                guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+                let config = GIDConfiguration(clientID: clientID)
+                GIDSignIn.sharedInstance.configuration = config
+
+                // Inicia el flujo de inicio de sesión con Google
+                GIDSignIn.sharedInstance.signIn(withPresenting: self) { [unowned self] result, error in
+                    if let error = error {
+                        print("Error al iniciar sesión con Google: \(error.localizedDescription)")
+                    } else if let user = result?.user,
+                              let idToken = user.idToken?.tokenString {
+                        let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: user.accessToken.tokenString)
+
+                        // Inicia sesión en Firebase con las credenciales de Google
+                        Auth.auth().signIn(with: credential) { _, firebaseError in
+                            if let firebaseError = firebaseError {
+                                print("Error al iniciar sesión en Firebase: \(firebaseError.localizedDescription)")
+                            } else {
+                                print("Inicio de sesión en Firebase exitoso")
+                                // Realiza cualquier acción adicional que necesites después del inicio de sesión.
+                            }
+                        }
+                    }
+                }
+        
+       
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
 
 }
